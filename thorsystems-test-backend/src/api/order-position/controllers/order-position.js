28acca0089d69ec07c08787582ecd482ab0c4128
RojkovD <rojkov.dmt@gmail.com>
2022-12-26 04:32:32 +0700

    backend added

---
+++ thorsystems-test-backend/src/api/order-position/controllers/order-position.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * order-position controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order-position.order-position');
